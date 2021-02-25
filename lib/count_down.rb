8b89c785847130560f7e189f7684126e3e3b3e46
Daniel Lee Beasley <dannyleebeasley@gmail.com>
2021-02-25 05:52:06 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,7 +1,7 @@
 magic_exit_number = 0
 count = 10 
 while count < 10 && count != magic_exit_number do
-  puts "I am the #{count}, I love to count!"
+  puts "#{count}"
   count += 1
 end
 # Write your code here
